624d332981a9627c0fa3d3fa0db2d79986cc53b8
raw9k <raunakgupta914@gmail.com>
2025-07-10 19:58:46 +0530

    exception and logging

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,4 +1,5 @@
 import sys
+from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
@@ -9,13 +10,15 @@ class NetworkSecurityException(Exception):
         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message"
-        self.file_name,self.lineno,str(self.error_message)
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name,self.lineno,str(self.error_message))
 
 
 if __name__ == "__main__":
     try:
+        logger.logging.info("entered the try block")
         a = 1/0
         print("This will not be printed", a)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
