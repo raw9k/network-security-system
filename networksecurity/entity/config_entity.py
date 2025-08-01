36240699d4fea0a3603f54825d5276692516a010
raw9k <raunakgupta914@gmail.com>
2025-08-01 19:53:08 +0530

    config_entity

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -16,7 +16,18 @@ class TrainingPipelineConfig:
 
 class DataIngestionConfig:
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
-        try:
-            pass
-        except:
-            pass
+        self.data_ingestion_dir:str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.DATA_INESTION_DIR_NAME
+        )
+        self.feature_store_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+        )
+        self.training_file_path:str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INESTION_INGESTED_DIR,training_pipeline.TRAIN_FILE_NAME
+        )
+        self.testing_file_path:str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+        )
+        self.train_test_spilt_ratio: float = training_pipeline.DATA_INESTION_TRAIN_TEST_SPLIT_RATIO
+        self.collection_name: str = training_pipeline.DATA_INESTION_COLLECTION_NAME
+        self.database_name:str = training_pipeline.DATA_INESTION_DATABASE_NAME
