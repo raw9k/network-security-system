74f8e5d3ca3a3fd2a208bc2ad0c1ad2487dd06e2
raw9k <raunakgupta914@gmail.com>
2025-08-16 03:28:47 +0530

    Integrates data validation and drift detection steps
    Adds data validation workflow after data ingestion,
    including schema checks and automated data drift detection.
    
    Persists drift reports and valid data files for downstream use.
    
    Improves modularity and error handling by introducing utility for writing YAML files.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 
@@ -10,10 +11,18 @@ if __name__ == "__main__":
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
-        logging.info("Initiate the data Ingestion")
         
+        logging.info("Initiate the data Ingestion")
         dataingestionartifact= data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        logging.info("Data Initiation Completed")
         
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifacts=dataingestionartifact)
+        logging.info("Initiate the data Validation")
+        data_validation_artifacts =data_validation.initiate_date_validation()
+        logging.info("Completed the data Validation")
+        
+        print(data_validation_artifacts)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -1,7 +1,7 @@
 from networksecurity.entity.artifact_entity import DataIngestionArtifacts, DataValidationArtifacts
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
-from networksecurity.utils.main_utils.utils import read_yml_file
+from networksecurity.utils.main_utils.utils import read_yml_file,write_yml_file
 
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
@@ -11,10 +11,10 @@ import os, sys, pandas as pd, numpy as np
 
 class DataValidation:
     def __init__(self,data_ingestion_artifacts:DataIngestionArtifacts,
-                 data_validation_artifacts: DataValidationArtifacts):
+                 data_validation_config: DataValidationConfig):
         try:
             self.data_ingestion_artifacts = data_ingestion_artifacts
-            self.data_validation_artifacts = data_validation_artifacts
+            self.data_validation_config = data_validation_config
             self._schema_config = read_yml_file(SCHEMA_FILE_PATH)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
@@ -28,7 +28,7 @@ class DataValidation:
         try:
             number_of_columns = len(self._schema_config)
             logging.info(f"Required Number of columns: {number_of_columns}")
-            logging.info(f"Data frame has {number_of_columns} columns")
+            logging.info(f"Data frame has {len(dataframe.columns)} columns")
             
             if len(dataframe.columns) == number_of_columns:
                 return True
@@ -47,7 +47,30 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
-        
+    def detect_data_drift(self, base_df,current_df, threshold = 0.05) -> bool:
+        try:
+            status = True
+            report ={}
+            for column in base_df.columns:
+                d1 = base_df[column]
+                d2 = current_df[column]
+                is_same_dist = ks_2samp(d1,d2)
+                if threshold <= is_same_dist.pvalue:
+                    is_found = False
+                else:
+                    is_found = True
+                    status = False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                }})
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            write_yml_file(file_path=drift_report_file_path, content=report)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+            
     def initiate_date_validation(self) -> DataValidationArtifacts:
         try:
             train_file_path = self.data_ingestion_artifacts.trained_file_path
@@ -77,6 +100,28 @@ class DataValidation:
             if not status:
                 error_message = "Test dataframe contains non-numeric columns.\n"
 
-                
+            # Checking for data drift
+            status = self.detect_data_drift(base_df=train_dataframe,current_df=test_dataframe)
+            
+            dir_path = os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path, index=False, header=True
+            )
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path, index=False, header=True
+            )
+            
+            data_validation_artifact = DataValidationArtifacts(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifacts.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifacts.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+            return data_validation_artifact
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
